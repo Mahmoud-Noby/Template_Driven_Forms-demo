acd07aff1cdcb024039a0cd8c469d01a97ceea6d
Mahmoud Noby <mahmoudnoby77@gmail.com>
2019-08-21 10:47:04 +0200

    launching app

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -9,11 +9,11 @@ import { UserSettings } from './app-data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   originalUserSettings : UserSettings = {
-    name: string="",
-    emailOffer:string,
-    interfaceStyle: string,
-    subscriptionType: string,
-    notes:string,
+    name: null ,
+    emailOffer: null,
+    interfaceStyle:  null,
+    subscriptionType:  null,
+    notes: null,
   }
   userSettings: UserSettings = {...this.originalUserSettings};
   constructor() { }
